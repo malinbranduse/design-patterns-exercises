6c1c15787710fe132f12cc386984e2ec0bbbda84
Mălin Brândușe <malin.branduse@gmail.com>
2018-06-20 17:11:13 -0400

    added factory pattern

---
+++ DesignPatterns/Factory/PersonFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Factory
+{
+    public class PersonFactory
+    {
+        private int IdCounter = 0;
+
+        public Person CreatePerson(string name)
+        {
+            return new Person { Id = IdCounter++, Name = name };
+        }
+    }
+}
---
+++ DesignPatterns/Factory/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Factory
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var personFactory = new PersonFactory();
+            var malin = personFactory.CreatePerson("Malin");
+            var steve = personFactory.CreatePerson("Steve");
+            Console.WriteLine(malin);
+            Console.WriteLine(steve);
+        }
+    }
+}
