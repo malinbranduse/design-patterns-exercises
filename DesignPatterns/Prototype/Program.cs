fa481e7f58dc98f582f5612b2b478f758ee04f3d
Mălin Brândușe <malin.branduse@gmail.com>
2018-06-21 16:35:51 -0400

    prototype test

--- DesignPatterns/Prototype/Program.cs
+++ DesignPatterns/Prototype/Program.cs
@@ -6,10 +6,10 @@ namespace Prototype
     {
         public static void Main(string[] args)
         {
-            var line = new Line() {
+            var line = new Line
+            {
                 Start = new Point() { X = 10, Y = 20 },
                 End = new Point() {X = 1, Y = 2},
-
             };
             var helpNeedLineCopy = line.DeepCopy();
         }
---
+++ DesignPatterns/Prototype/PrototypeTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Prototype
+{
+    public class PrototypeTest
+    {
+        [Test()]
+        public void TestCase()
+        {
+            var line = new Line
+            {
+                Start = new Point() { X = 10, Y = 20 },
+                End = new Point() { X = 1, Y = 2 },
+            };
+            var helpNeedLineCopy = line.DeepCopy();
+            Assert.AreEqual(line.Start.X, helpNeedLineCopy.Start.X);
+            Assert.AreEqual(line.Start.Y, helpNeedLineCopy.Start.Y);
+
+            Assert.AreEqual(line.End.X, helpNeedLineCopy.End.X);
+            Assert.AreEqual(line.End.Y, helpNeedLineCopy.End.Y);
+        }
+    }
+}
