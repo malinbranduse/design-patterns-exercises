076bdf652ff068ad1a1c2822c2c8642c6ae175e6
Mălin Brândușe <malin.branduse@gmail.com>
2018-06-27 17:39:26 +0300

    simple proxy

---
+++ DesignPatterns/Proxy/ProxyTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+namespace Proxy
+{
+    public class ProxyTest
+    {
+        [Test()]
+        public void TestCase()
+        {
+            var p = new Person { Age = 17 };
+            var rp = new ResponsiblePerson(p);
+            Assert.AreNotSame(p.Drink(), rp.Drink());
+            Assert.AreSame(p.Drive(), rp.Drive());
+            Assert.AreNotSame(p.DrinkAndDrive(), rp.DrinkAndDrive());
+        }
+    }
+}
---
+++ DesignPatterns/Proxy/ResponsiblePerson.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Proxy
+{
+    public class ResponsiblePerson
+    {
+        private Person person;
+
+        public ResponsiblePerson(Person person)
+        {
+            this.person = person;
+        }
+
+        public string Drink()
+        {
+            return person.Age >= 18
+                ? person.Drink()
+                : "too young";
+        }
+
+        public string Drive()
+        {
+            return person.Age >= 16
+                ? person.Drive()
+                : "too young";
+        }
+
+        public string DrinkAndDrive()
+        {
+            return "dead";
+        }
+    }
+}
